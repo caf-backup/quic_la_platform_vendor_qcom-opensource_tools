1cde1d1d6d7f62e51462a6ec00607cffecea7664
Yashwanth <yvulapu@codeaurora.org>
2020-08-05 16:38:25 +0530

    lrdp-v2: update parsing logic for sde event logs & register dumps
    This change updates logic to parse sde eventlogs & register dumps.
    
    Change-Id: I20c936dc4874517ad29b8942f04fb46e251e7d7f

--- linux-ramdump-parser-v2/parsers/mdpinfo.py
+++ linux-ramdump-parser-v2/parsers/mdpinfo.py
@@ -80,25 +80,26 @@ def get_u64(self, key):
         address = self.get_address(key)
         return self.ramdump.read_u64(address)
 
-@register_parser('--print-mdpinfo', 'print mdp info')
+@register_parser('--print-mdssinfo', 'print mdss info')
 class MDPinfo(RamParser):
     def __init__(self, *args):
         super(MDPinfo, self).__init__(*args)
         self.outfile = None
 
-    def mdss_dump_reg(self, addr, length, reg_dump):
+    def mdss_dump_reg(self, addr, length, reg_dump, offset):
         if reg_dump == 0:
             return
 
         # Making length multiple of 16
-        length = (length + 15) // 16
+        length = int((length + 15) / 16)
+        base = addr
 
         # Print out registers
         for i in range(0, length):
-            self.outfile.write('{0:x} : '.format(addr))
+            self.outfile.write('0x{0:x}| '.format(addr-base+offset, 10))
             for j in range(0, 4):
                 read = reg_dump + (16 * i) + (4 * j)
-                self.outfile.write('{0:#0{1}x} '
+                self.outfile.write('{0:08x} '
                                    .format(self.ramdump.read_u32(read), 10))
 
             self.outfile.write('\n')
@@ -123,7 +124,7 @@ class MDPinfo(RamParser):
         # Calculating length
         length = min(blk.max_offset, rng.offset.end) - rng.offset.start
 
-        self.mdss_dump_reg(addr, length, rng.reg_dump)
+        self.mdss_dump_reg(addr, length, rng.reg_dump, rng.offset.start)
 
     def print_sderange(self, node):
         rng = RangeDumpSdeNode(self.ramdump, node)
@@ -135,17 +136,14 @@ class MDPinfo(RamParser):
 
         addr = node + rng.offset.start
 
-        self.outfile.write('{0}: base=0x{1:x} start=0x{2:x} end=0x{3:x}\n'
-                           .format(rng.range_name, addr,
-                                   rng.offset.start, rng.offset.end))
-        self.outfile.write('start_addr:{0:x} end_addr:{1:x} reg_addr={2:x}\n'
-                           .format(rng.offset.start, rng.offset.end, addr))
-
+        self.outfile.write('\n---------------------------------------------------------------------\n')
+        self.outfile.write('[{0}]  0x{1:x}\n'.format(rng.range_name, rng.offset.start))
+        self.outfile.write('---------------------------------------------------------------------\n')
         # Calculating length
 
         length =  rng.offset.end - rng.offset.start
 
-        self.mdss_dump_reg(addr, length, rng.reg_dump)
+        self.mdss_dump_reg(addr, length, rng.reg_dump, rng.offset.start)
 
     def parse(self):
 
@@ -155,7 +153,7 @@ class MDPinfo(RamParser):
             mdss_dbg = SdeDbgBase(self.ramdump, 'sde_dbg_base')
             if mdss_dbg.is_empty():
                 return
-            self.outfile = self.ramdump.open_file('MDPINFO_OUT.txt')
+            self.outfile = self.ramdump.open_file('sde_regdump.txt')
             dump_list = ListWalker(self.ramdump, mdss_dbg.reg_base_list, 0)
             if dump_list.is_empty():
                 self.outfile.write('%s \n' % ("BLK DUMPLIST IS EMPTY!!!"))
@@ -169,54 +167,73 @@ class MDPinfo(RamParser):
                             'reg_dump': Struct.get_pointer})
                     if (reg_blk.base == 0x0000):
                             continue
-                    self.outfile.write('%s %x \n' % ("BLK is:", blk))
-                    self.outfile.write('%s  %s\n' % ("REG BLK is:", reg_blk.name))
+                    self.outfile.write('\n---------------------------------------------------------------------\n')
+                    self.outfile.write('[%s]   0x%x\n' % (reg_blk.name, reg_blk.base))
+                    self.outfile.write('---------------------------------------------------------------------\n')
                     headoffset_2 = self.ramdump.field_offset('struct sde_dbg_reg_range', 'head')
                     sub_blk_list = ListWalker(self.ramdump, reg_blk.sub_range_list, headoffset_2)
 
                     if sub_blk_list.is_empty():
-                        self.outfile.write('Ranges not found, ''will dump full registers\n')
-                        self.outfile.write('base:0x%x length:%d\n' % (reg_blk.base, reg_blk.max_offset))
-                        self.mdss_dump_reg(reg_blk.base, reg_blk.max_offset, reg_blk.reg_dump)
+                        self.mdss_dump_reg(reg_blk.base, reg_blk.max_offset, reg_blk.reg_dump, 0)
                     else:
                         for node in sub_blk_list:
                             self.print_sderange(node)
 
             # EVENT LOGS
-            self.outfile = self.ramdump.open_file('MDP_EVENT_LOGS.txt')
+            self.outfile = self.ramdump.open_file('sde_evtlog.txt')
 
             evt_log = Struct(self.ramdump, mdss_dbg.evtlog,
                       struct_name="struct sde_dbg_evtlog",
                       fields={'name': Struct.get_cstring,
                               'enable': Struct.get_u32,
+                              'last': Struct.get_u32,
+                              'curr': Struct.get_u32,
                               'logs': Struct.get_address,
                               'last_dump': Struct.get_u32})
 
-            SDE_EVTLOG_ENTRY = self.ramdump.sizeof('struct sde_dbg_evtlog') // self.ramdump.sizeof('struct sde_dbg_evtlog_log')
-            SDE_EVTLOG_MAX_DATA = self.ramdump.sizeof('(*(sde_dbg_base.evtlog)).logs[0].data') // 4
-            self.outfile.write('\n %s %40s:%s %35s  %10s %30s \n ' % ("TIME", "NAME", "LINE", "PID", "DATA_CNT", "DATA"))
-
-            for i in range(SDE_EVTLOG_ENTRY):
+            SDE_EVTLOG_ENTRY = 8192
+            SDE_EVTLOG_MAX_DATA = self.ramdump.sizeof('(*(sde_dbg_base.evtlog)).logs[0].data')/4
+            self.outfile.write('%-60.50s%-20.5s%-8.5s%-8.5s%s\n' % ("FUNC", "TIME", "PID", "CPU", "DATA"))
+            sde_evtlog_start = 0
+            sde_evtlog_repeat = 0
+            if (evt_log.curr != evt_log.last):
+                    sde_evtlog_start = evt_log.curr
+            else:
+                    sde_evtlog_repeat = 1
+            sde_evtlog_count = 0
+            while(sde_evtlog_count < SDE_EVTLOG_ENTRY):
+                    i = sde_evtlog_start % SDE_EVTLOG_ENTRY
+                    if (i == evt_log.curr):
+                            if (sde_evtlog_repeat):
+                                    break
+                            else:
+                                    sde_evtlog_repeat = 1
                     addr = evt_log.logs + self.ramdump.sizeof('struct sde_dbg_evtlog_log') * i
                     log_log = addr
+                    sde_evtlog_start = sde_evtlog_start + 1
+                    sde_evtlog_count = sde_evtlog_count + 1
                     log_log = Struct(self.ramdump, log_log,
                                      struct_name="struct sde_dbg_evtlog_log",
                                      fields={'name': Struct.get_pointer,
-                                             'enable': Struct.get_u32,
+                                             'cpu': Struct.get_u32,
                                              'line': Struct.get_u32,
                                              'data_cnt': Struct.get_u32,
                                              'pid': Struct.get_u32,
                                              'data' : Struct.get_address,
                                              'time': get_u64})
 
+                    if (log_log.time == 0x0):
+                            break
+                    func_name = self.ramdump.read_cstring(log_log.name)
+                    func_name = func_name + ':' + str(log_log.line)
+                    self.outfile.write('%-60.50s' % (func_name))
+                    self.outfile.write('%-20.0d' % (log_log.time))
+                    self.outfile.write('%-8.1d' % (log_log.pid))
+                    self.outfile.write('%-8.1d' % (log_log.cpu))
+                    for i in range(log_log.data_cnt):
+                            self.outfile.write('%x ' % (self.ramdump.read_u32(log_log.data+(i*4))))
                     self.outfile.write('\n' % ())
-                    self.outfile.write('%d    ' % (log_log.time))
-                    self.outfile.write('%50s:' % (self.ramdump.read_cstring(log_log.name)))
-                    self.outfile.write('%s%d \t\t ' % ("", log_log.line))
-                    self.outfile.write('%10d    ' % (log_log.pid))
-                    self.outfile.write('%6d       ' % (log_log.data_cnt))
-                    for i in range(SDE_EVTLOG_MAX_DATA):
-                            self.outfile.write('%2d ' % (self.ramdump.read_u32(log_log.data+(i*4))))
+
 
         else:
             for blk in mdss_dbg.blk_arr:
@@ -240,7 +257,7 @@ class MDPinfo(RamParser):
                         self.outfile.write('base:0x%x length:%d\n' %
                                            (blk.base, blk.max_offset))
 
-                        self.mdss_dump_reg(blk.base, blk.max_offset, blk.reg_dump)
+                        self.mdss_dump_reg(blk.base, blk.max_offset, blk.reg_dump, 0)
                 else:
                     for node in dump_list:
                         self.print_range(blk, node)
