caf-backup/quic_la_platform_vendor_qcom-opensource_tools

commit e307b274a24216eb6c9431d20a0c694af82917cb

ramdump.py: Load modules with section offsets
GDB loads modules with the following command:
  add-symbol-file <filename> <text offset>

<text offset> provides an address to load .text at, but does not
relocate .data, .bss, or any other section. These sections must be
specifically mentioned with extra arguments of the form "-s <segment>
<address>". The address must be the correct load address, and not an
offset.

To calculate load addresses of each section, use elftools to extract
section names and offsets. Note that as of 5.10 kernel, KASLR doesn't
internally shift module sections, just the base offest of that module,
so adding the module's base address to each section offset is sufficient
to calculate that section's load address.

Change-Id: If1a4e49e728441fd7b03b3e29181013e9edc1187

linux-ramdump-parser-v2/ramdump.py
@@ -1425,6 +1425,13 @@ class RamDump():
                     self.kaslr_offset = self.read_u64(self.kaslr_addr + 4, False)
                     print_out_str("The kaslr_offset extracted is: " + str(hex(self.kaslr_offset)))
 
+    def get_page_size(self):
+        if self.is_config_defined('CONFIG_ARM64_PAGE_SHIFT'):
+            PAGE_SHIFT = int(self.get_config_val('CONFIG_ARM64_PAGE_SHIFT'))
+        else:
+            PAGE_SHIFT = 12
+        return 1 << PAGE_SHIFT
+
     def get_hw_id(self, add_offset=True):
         socinfo_format = -1
         socinfo_id = -1
@@ -1668,6 +1675,56 @@ class RamDump():
         if not mod_tbl_ent.set_sym_path(ko_file_list[mod_tbl_ent.name]):
             return
 
+        try:
+            mod = import_module('elftools.elf.elffile')
+            ELFFile = mod.ELFFile
+            mod = import_module('elftools.elf.constants')
+            SHF = mod.SH_FLAGS
+            ARCH_SHF_SMALL = 0  # 0 for arm and arm64
+            SHF_RO_AFTER_INIT = 0x00200000  # custom for Linux
+            with open(mod_tbl_ent.get_sym_path(), 'rb') as fd:
+                elf = ELFFile(fd)
+                # This logic mirrors kernel/module.c:layout_sections and layout_and_allocate
+                # https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git/tree/kernel/module.c?h=v5.13#n2425
+                masks = [
+                    (SHF.SHF_EXECINSTR | SHF.SHF_ALLOC, ARCH_SHF_SMALL),
+                    (SHF.SHF_ALLOC, SHF.SHF_WRITE | ARCH_SHF_SMALL),
+                    (SHF_RO_AFTER_INIT | SHF.SHF_ALLOC, ARCH_SHF_SMALL),
+                    (SHF.SHF_WRITE | SHF.SHF_ALLOC, ARCH_SHF_SMALL),
+                    (ARCH_SHF_SMALL | SHF.SHF_ALLOC, 0)
+                ]
+                size = 0
+                mod_tbl_ent.section_offsets = {}
+                for mask in masks:
+                    for s in elf.iter_sections():
+                        sh_flags = s['sh_flags']
+                        # https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git/tree/kernel/module.c?h=v5.13#n3524
+                        if s.name == '.data..ro_after_init':
+                            sh_flags |= SHF_RO_AFTER_INIT
+                        if s.name == '__jump_table':
+                            sh_flags |= SHF_RO_AFTER_INIT
+                        # https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git/tree/kernel/module.c?h=v5.13#n2442
+                        if (sh_flags & mask[0]) != mask[0] \
+                            or (sh_flags & mask[1]) \
+                            or s.name in mod_tbl_ent.section_offsets.keys() \
+                            or s.name.startswith('.init') or s.name.startswith('.ARM.extab.init') or s.name.startswith('.ARM.exidx.init'):
+                            continue
+                        # this is internals of get_offset -- arch_mod_section_prepend not defined for arm or arm64 so far
+                        # https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git/tree/kernel/module.c?h=v5.13#n2397
+                        if s['sh_addralign']:
+                            size = (size + s['sh_addralign'] - 1) & ~(s['sh_addralign'] - 1)
+
+                        if s.name:
+                            # Add to section_offsets table!
+                            mod_tbl_ent.section_offsets[s.name] = size + mod_tbl_ent.module_offset
+                        size += s['sh_size']
+                    # This is the internals of debug_align
+                    # https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git/tree/kernel/module.c?h=v5.13#n2456
+                    if self.is_config_defined('CONFIG_ARCH_HAS_STRICT_MODULE_RWX'):
+                        size = (size + (self.get_page_size() - 1)) & ~(self.get_page_size() - 1)
+        except ImportError:
+            pass
+
         args = [self.nm_path, '-n', mod_tbl_ent.get_sym_path()]
         p = subprocess.run(args, stdout=subprocess.PIPE)
         symbols = p.stdout.decode().splitlines()
@@ -1819,26 +1876,8 @@ class RamDump():
         >>> hex(dump.address_of('linux_banner'))
         '0xffffffc000c7a0a8L'
         """
-        flag = False
-        sym_name = symbol
         try:
-            addr = self.gdbmi.address_of(symbol)
-            if ((addr & 0xFF000000000000) == 0) and self.arm64:
-                for mod_tbl_ent in self.lookup_table:
-                    if "." in symbol:
-                        sym_name = symbol.split(".")[0]
-                        var = ".".join(symbol.split(".")[1:])
-                        sym_type = self.type_of(sym_name)
-                        var_offset = self.field_offset(sym_type, var)
-                        flag = True
-                    if sym_name in str(mod_tbl_ent) and sym_name == mod_tbl_ent[2]:
-                        addr = mod_tbl_ent[0]
-                if flag:
-                    return addr + var_offset
-                else:
-                    return addr
-            else:
-                return addr
+            return self.gdbmi.address_of(symbol)
         except gdbmi.GdbMIException:
             if self.hyp:
                 try:

linux-ramdump-parser-v2/gdbmi.py
@@ -117,11 +117,13 @@ class GdbMI(object):
 
     def setup_module_table(self, module_table):
         self.mod_table = module_table
-        d = {"\\":"\\\\"}
-        for mod_tbl_ent in self.mod_table.module_table:
-            load_mod_sym_cmd = 'add-symbol-file %s 0x%x' % (mod_tbl_ent.get_sym_path(), mod_tbl_ent.module_offset)
-            gdb_cmd = ''.join(d.get(c, c) for c in load_mod_sym_cmd)
-            self._run(gdb_cmd)
+        for mod in self.mod_table.module_table:
+            if not mod.get_sym_path():
+                continue
+            load_mod_sym_cmd = ['add-symbol-file', mod.get_sym_path().replace('\\', '\\\\'), '0x{:x}'.format(mod.module_offset - self.kaslr_offset)]
+            for segment, offset in mod.section_offsets.items():
+                load_mod_sym_cmd += ['-s', segment, '0x{:x}'.format(offset - self.kaslr_offset) ]
+            self._run(' '.join(load_mod_sym_cmd))
 
     def _run(self, cmd, skip_cache=False, save_in_cache=True):
         """Runs a gdb command and returns a GdbMIResult of the result. Results

linux-ramdump-parser-v2/module_table.py
@@ -21,6 +21,7 @@ class module_table_entry():
         self.sym_path = ''
         self.kallsyms_addr = 0
         self.kallsyms_table = []
+        self.section_offsets = {}
 
     def num_symbols(self):
         return len(self.sym_lookup_table)

commit 2d8ac90f4f685973da8a4143442667339d6aa30a

ramdump: Simplify module section layout
Previous parsing for determining module layout lacked the frobbing[1] which
happens by the linux kernel when loading modules. Specifically, .plt
section was not grown to correct size. As a result, large modules which
have many relocations would not have the same section layout that kernel
had assigned.

Instead, use module_sect_attr in module struct. This has limitation that
minidump parsing does not yield give complete location of module
symbols. This is not an issue for minidump because virtual address can
be found by analyze minidump elf section data (see commit 9b0aa6688e7b
("parsers: watchdog: Add module+minidump support")).

Change-Id: Ie9f4327af00be734614a4ae979972c77f0edcad6
Fixes: e307b274a242 ("ramdump.py: Load modules with section offsets")

linux-ramdump-parser-v2/ramdump.py
@@ -1644,22 +1644,45 @@ class RamDump():
 
         if self.kernel_version > (4, 9, 0):
             module_core_offset = self.field_offset('struct module', 'core_layout.base')
+            sect_name_offset = self.field_offset('struct module_sect_attr', 'battr') + self.field_offset('struct bin_attribute', 'attr') + self.field_offset('struct attribute', 'name')
         else:
             module_core_offset = self.field_offset('struct module', 'module_core')
+            sect_name_offset = self.field_offset('struct module_sect_attr', 'name')
 
         kallsyms_offset = self.field_offset('struct module', 'kallsyms')
+        sect_addr_offset = self.field_offset('struct module_sect_attr', 'address')
+        nsections_offset = self.field_offset('struct module_sect_attrs', 'nsections')
+        section_attrs_offset = self.field_offset('struct module_sect_attrs', 'attrs')
+        section_attr_size = self.sizeof('struct module_sect_attr')
+        mod_sect_attrs_offset = self.field_offset('struct module', 'sect_attrs')
 
         next_list_ent = self.read_pointer(mod_list + next_offset)
         while next_list_ent and next_list_ent != mod_list:
-            mod_tbl_ent = module_table.module_table_entry()
             module = next_list_ent - list_offset
-            name_ptr = module + name_offset
-            mod_tbl_ent.name = self.read_cstring(name_ptr)
+
+            mod_tbl_ent = module_table.module_table_entry()
+            mod_tbl_ent.name = self.read_cstring(module + name_offset)
             mod_tbl_ent.module_offset = self.read_pointer(module + module_core_offset)
             if mod_tbl_ent.module_offset is None:
                 mod_tbl_ent.module_offset = 0
             mod_tbl_ent.kallsyms_addr = self.read_pointer(module + kallsyms_offset)
+            # Loop through sect_attrs
+            mod_tbl_ent.section_offsets = {}
+            mod_sect_attrs = self.read_pointer(module + mod_sect_attrs_offset)  # module.sect_attrs
+            for i in range(0, self.read_u32(mod_sect_attrs + nsections_offset)):
+                # attr_ptr = module.sect_attrs.attrs[i]
+                attr_ptr = mod_sect_attrs + section_attrs_offset + (i * section_attr_size)
+                # sect_name = attr_ptr.battr.attr.name (for 5.4+)
+                sect_name = self.read_cstring(self.read_pointer(attr_ptr + sect_name_offset))
+                # sect_addr = attr_ptr.address
+                sect_addr = self.read_word(attr_ptr + sect_addr_offset)
+                # https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git/tree/scripts/gdb/linux/symbols.py?h=v5.14#n102
+                if sect_name not in ['.data', '.data..read_mostly', '.rodata', '.bss',
+                                     '.text', '.text.bss', '.text.hot', '.text.unlikely']:
+                    continue
+                mod_tbl_ent.section_offsets[sect_name] = sect_addr
             self.module_table.add_entry(mod_tbl_ent)
+
             next_list_ent = self.read_pointer(next_list_ent + next_offset)
 
     def retrieve_minidump_modules(self):
@@ -1688,56 +1711,6 @@ class RamDump():
             ko_file_list[mod_tbl_ent.name] = temp_data
         if not mod_tbl_ent.set_sym_path(ko_file_list[mod_tbl_ent.name]):
             return
-
-        try:
-            mod = import_module('elftools.elf.elffile')
-            ELFFile = mod.ELFFile
-            mod = import_module('elftools.elf.constants')
-            SHF = mod.SH_FLAGS
-            ARCH_SHF_SMALL = 0  # 0 for arm and arm64
-            SHF_RO_AFTER_INIT = 0x00200000  # custom for Linux
-            with open(mod_tbl_ent.get_sym_path(), 'rb') as fd:
-                elf = ELFFile(fd)
-                # This logic mirrors kernel/module.c:layout_sections and layout_and_allocate
-                # https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git/tree/kernel/module.c?h=v5.13#n2425
-                masks = [
-                    (SHF.SHF_EXECINSTR | SHF.SHF_ALLOC, ARCH_SHF_SMALL),
-                    (SHF.SHF_ALLOC, SHF.SHF_WRITE | ARCH_SHF_SMALL),
-                    (SHF_RO_AFTER_INIT | SHF.SHF_ALLOC, ARCH_SHF_SMALL),
-                    (SHF.SHF_WRITE | SHF.SHF_ALLOC, ARCH_SHF_SMALL),
-                    (ARCH_SHF_SMALL | SHF.SHF_ALLOC, 0)
-                ]
-                size = 0
-                mod_tbl_ent.section_offsets = {}
-                for mask in masks:
-                    for s in elf.iter_sections():
-                        sh_flags = s['sh_flags']
-                        # https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git/tree/kernel/module.c?h=v5.13#n3524
-                        if s.name == '.data..ro_after_init':
-                            sh_flags |= SHF_RO_AFTER_INIT
-                        if s.name == '__jump_table':
-                            sh_flags |= SHF_RO_AFTER_INIT
-                        # https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git/tree/kernel/module.c?h=v5.13#n2442
-                        if (sh_flags & mask[0]) != mask[0] \
-                            or (sh_flags & mask[1]) \
-                            or s.name in mod_tbl_ent.section_offsets.keys() \
-                            or s.name.startswith('.init') or s.name.startswith('.ARM.extab.init') or s.name.startswith('.ARM.exidx.init'):
-                            continue
-                        # this is internals of get_offset -- arch_mod_section_prepend not defined for arm or arm64 so far
-                        # https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git/tree/kernel/module.c?h=v5.13#n2397
-                        if s['sh_addralign']:
-                            size = (size + s['sh_addralign'] - 1) & ~(s['sh_addralign'] - 1)
-
-                        if s.name:
-                            # Add to section_offsets table!
-                            mod_tbl_ent.section_offsets[s.name] = size + mod_tbl_ent.module_offset
-                        size += s['sh_size']
-                    # This is the internals of debug_align
-                    # https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git/tree/kernel/module.c?h=v5.13#n2456
-                    if self.is_config_defined('CONFIG_ARCH_HAS_STRICT_MODULE_RWX'):
-                        size = (size + (self.get_page_size() - 1)) & ~(self.get_page_size() - 1)
-        except ImportError:
-            pass
 
         args = [self.nm_path, '-n', mod_tbl_ent.get_sym_path()]
         p = subprocess.run(args, stdout=subprocess.PIPE)