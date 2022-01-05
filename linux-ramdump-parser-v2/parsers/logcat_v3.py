6571752d370557bd1159e48fdd124a2224d0137a
Zhiqing Xiong <quic_zhiqxion@quicinc.com>
2022-01-05 15:06:42 +0800

    Lrdp2: Adjust log time according to local timezone
    1. UTC log time is stored in ringbuffer in logd, if print
       time need adjust with local timezone
    2. Add line prefix if single log has more lines
    3. Remove unused parameters
    
    Change-Id: Id381bc5278f8d5a188b8dc5d0a8e70f02a0ce530
    CRs-Fixed: 3104652

--- linux-ramdump-parser-v2/parsers/logcat_v3.py
+++ linux-ramdump-parser-v2/parsers/logcat_v3.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2021 Qualcomm Innovation Center, Inc. All rights reserved.
+# Copyright (c) 2021-2022 Qualcomm Innovation Center, Inc. All rights reserved.
 
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License version 2 and
@@ -72,6 +72,12 @@ class Logcat_base(RamParser):
             self.addr_length    = 8
         else:
             self.addr_length    = 4
+        self.extra_offset = 0
+        sys_tz_addr = self.ramdump.address_of('sys_tz')
+        tz_minuteswest_offset = self.ramdump.field_offset(
+                    'struct timezone ', 'tz_minuteswest')
+        self.tz_minuteswest = self.ramdump.read_s32(sys_tz_addr + tz_minuteswest_offset)
+        print_out_str("struct timezone --> tz_minuteswest= "+str(self.tz_minuteswest)+"min")
 
     def read_bytes(self, addr, len):
         addr = self.mmu.virt_to_phys(addr)
@@ -115,6 +121,7 @@ class Logcat_base(RamParser):
     def format_time(self, tv_sec, tv_nsec):
         tv_nsec = str(tv_nsec // 1000000)
         tv_nsec = str(tv_nsec).zfill(3)
+        tv_sec -= 60 * self.tz_minuteswest
         date = datetime.datetime.utcfromtimestamp(tv_sec)
         timestamp = date.strftime("%m-%d %H:%M:%S") + '.' + tv_nsec
         return timestamp
@@ -165,7 +172,7 @@ class Logcat_base(RamParser):
             msg = tmpmsg.decode('ascii', 'ignore').strip()
         return evt_type, msg, length
 
-    def process_log_and_save(self, _data, log_id, section):
+    def process_log_and_save(self, _data):
         ret=""
         pos = 0
         while pos < len(_data):
@@ -173,7 +180,7 @@ class Logcat_base(RamParser):
                 break
             # first [0-30] total 31 bytes
             logEntry = struct.unpack('<IIIQIIHB', _data[pos:pos+self.SIZEOF_LOG_ENTRY+1])
-            pos = pos+self.SIZEOF_LOG_ENTRY+1
+            pos = pos+self.SIZEOF_LOG_ENTRY + 1 + self.extra_offset
             uid = logEntry[0]
             pid = logEntry[1]
             tid = logEntry[2]
@@ -189,13 +196,19 @@ class Logcat_base(RamParser):
             pos = pos+msg_len-1
             timestamp = self.format_time(tv_sec, tv_nsec)
             if len(msgList) >= 2 :
-                fmt_msg = "%s %5d %5d %5d %c %-8.*s: %s\n" % (
+                multilines = msgList[1].split("\n")
+                preifx_line = "%s %5d %5d %5d %c %-8.*s: " % (
                     timestamp, uid, pid, tid, self.filter_pri_to_char(priority), len(msgList[0]),
-                    cleanupString(msgList[0].strip()),cleanupString(msgList[1].strip()))
-                ret += fmt_msg
+                    cleanupString(msgList[0].strip()))
+                if len(multilines) > 1:
+                    for line in multilines:
+                        if(len(cleanupString(line.strip())) > 0):
+                            ret += preifx_line + "%s\n" % cleanupString(line)
+                else:
+                    ret += preifx_line + "%s\n" % cleanupString(msgList[1].strip())
         return ret
 
-    def process_binary_log_and_save(self, _data, log_id=0, section =0):
+    def process_binary_log_and_save(self, _data):
         ret=""
         pos = 0
         while pos < len(_data):
@@ -203,7 +216,7 @@ class Logcat_base(RamParser):
                 break
             # first [0-30] total 31 bytes
             logEntry = struct.unpack('<IIIQIIH', _data[pos : pos + self.SIZEOF_LOG_ENTRY])
-            pos = pos + self.SIZEOF_LOG_ENTRY
+            pos = pos + self.SIZEOF_LOG_ENTRY + self.extra_offset
             uid = logEntry[0]
             pid = logEntry[1]
             tid = logEntry[2]
@@ -256,9 +269,9 @@ class Logcat_base(RamParser):
         ret = None
         if _data:
             if is_binary:
-                ret = self.process_binary_log_and_save(_data, log_id, section)
+                ret = self.process_binary_log_and_save(_data)
             else:
-                ret = self.process_log_and_save(_data, log_id, section)
+                ret = self.process_log_and_save(_data)
 
         return log_id, section, ret
 
@@ -530,7 +543,6 @@ class Logcat_vma(Logcat_base):
         print("LogBuffer address",hex(chunklist_addr-0x60))
         self.process_chunklist_and_save(chunklist_addr)
         print_out_str("logbuf_addr = 0x%x" %(chunklist_addr-0x60))
-        self.process_chunklist_and_save(chunklist_addr)
         print("logcat_vma parse logcat cost "+str((datetime.datetime.now()-startTime).total_seconds())+" s")
         return self.is_success
 
